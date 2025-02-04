a8ae8e2f886ab39e2cabce59c50776ead84d7512
KCAU-Tech <tech@students.kcau.ac.ke>
2025-02-04 23:23:51 +0300

    First commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
-"use client"
+"use client";
 
 export default function Home() {
   return (
     <div>
-      <h1>
-        KCAU Bookface
-      </h1>
+      <h1>KCAU Bookface</h1>
+      <p>
+        KCAU Bookface is a platform designed to enhance student interactions at
+        KCA University.
+      </p>
     </div>
   );
 }
